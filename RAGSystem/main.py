3b41cb68125c0f3034731c94f4d705cabae80aaa
Axel Friman <axelfriman04@gmail.com>
2025-09-30 14:30:33 +0200

    Added toml file for easy installment

--- RAGSystem/main.py
+++ RAGSystem/main.py
@@ -5,20 +5,6 @@ from llm import init_llm, load_prompt
 
 # Code inspired by LangChain tutorial on RAG https://python.langchain.com/docs/tutorials/rag
 
-'''
-To run code in Visual Studio (2022)
-* Find path to Python Environment
-    1. View -> Other Windows -> Python Environments
-    2. Get PATH
-* Install packages
-    1. Open terminal (View -> Terminal)
-    2. Run "PATH -m pip install beautifulsoup4 langchain-community"
-        * PATH -m pip show beautifulsoup4 (Check if it worked)
-    3. Run "PATH -m pip install sentence-transformers"
-        * "PATH -m pip show sentence-transformers"
-    4. Run "PATH -m pip install langgraph"
-'''
-
 
 # Load and split documents
 all_splits = load_and_split("https://en.wikipedia.org/wiki/Boletus_edulis")
